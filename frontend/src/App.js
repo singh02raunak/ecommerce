76888bb7a853a10ba8c6edd54c9e6cff84366eac
raunak <singh02raunak@example.com>
2020-10-06 21:43:10 +0530

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>hello everyone</h1>
+    </>
+  );
+}
+
+export default App;
